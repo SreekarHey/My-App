b695c1272a280c1800a4e9ff25dfe9a3ba035bed
SreekarManchali <manchali.sreekar@estuate.com>
2021-12-28 23:34:11 +0530

    Firstbranch commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className="App">Hi There</div>;
+  return <div className="App">Hi There, How You Doing !!!</div>;
 }
 
 export default App;
